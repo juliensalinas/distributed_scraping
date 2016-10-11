88d75fa8d73449e57c133f3df07228f025de25b8
juliensalinas <julien.enilrahc@gmail.com>
2016-10-11 14:58:30 +0200

    Add files via upload

--- comptes_clients/models.py
+++ comptes_clients/models.py
@@ -11,47 +11,47 @@ from django.contrib.auth.models import User
 
 class Client(models.Model):
 
-	Utilisateur = models.OneToOneField(User)
-	Email_hashe = models.CharField(max_length=100,blank=True, null=True)
-	Ip_inscription = models.CharField(max_length=30,blank=True, null=True)
-	Telephone = models.CharField(max_length=30,blank=True, null=True)
-	Poste_occupe = models.CharField(max_length=100,blank=True, null=True)
-	Date_inscription = models.DateTimeField()
-	Raison_sociale_entreprise = models.CharField(max_length=100,blank=True, null=True)
-	Siren_entreprise = models.CharField(max_length=10,blank=True, null=True)
+	utilisateur = models.OneToOneField(User)
+	email_hashe = models.CharField(max_length=100,blank=True, null=True)
+	ip_inscription = models.CharField(max_length=30,blank=True, null=True)
+	telephone = models.CharField(max_length=30,blank=True, null=True)
+	poste_occupe = models.CharField(max_length=100,blank=True, null=True)
+	date_inscription = models.DateTimeField()
+	raison_sociale_entreprise = models.CharField(max_length=100,blank=True, null=True)
+	siren_entreprise = models.CharField(max_length=10,blank=True, null=True)
 
 class Email(models.Model):
 
-	Fk_Client_id = models.IntegerField()
-	Email = models.CharField(max_length=100)
+	fk_Client_id = models.IntegerField()
+	email = models.CharField(max_length=100)
 
 class Telephone(models.Model):
 
-	Fk_Client_id = models.IntegerField()
-	Telephone = models.CharField(max_length=30)
+	fk_Client_id = models.IntegerField()
+	telephone = models.CharField(max_length=30)
 
 class Scraper(models.Model):
 
-	Fk_Client_id = models.IntegerField()
-	Nom_scraper = models.CharField(max_length=100)
-	Libelle_commercial_scraper = models.CharField(max_length=1000)
-	Notification_email = models.BooleanField()
-	Notification_csv_email = models.BooleanField()
-	Notification_xls_email = models.BooleanField()
-	Notification_sms = models.BooleanField()
-	Frequence_alertes = models.CharField(max_length=20)
-	Structure_page_scrapee = models.CharField(max_length=10000)
+	fk_Client_id = models.IntegerField()
+	nom_scraper = models.CharField(max_length=100)
+	libelle_commercial_scraper = models.CharField(max_length=1000)
+	notification_email = models.BooleanField()
+	notification_csv_email = models.BooleanField()
+	notification_xls_email = models.BooleanField()
+	notification_sms = models.BooleanField()
+	frequence_alertes = models.CharField(max_length=20)
+	structure_page_scrapee = models.CharField(max_length=10000)
 
 class Donnee_scrapee(models.Model):
 
-	Fk_Client_id = models.IntegerField()
-	Fk_Scrapers_id = models.IntegerField()
-	Abscisse = models.IntegerField()
-	Ordonnee = models.IntegerField()
-	Type = models.CharField(max_length=20)
-	Nom = models.CharField(max_length=100)
-	Contenu = models.CharField(max_length=10000)
-	Envoyer_notification = models.BooleanField()
-	Afficher_comme_nouveau = models.BooleanField()
-	Date_extraction = models.DateTimeField()
+	fk_Client_id = models.IntegerField()
+	fk_Scrapers_id = models.IntegerField()
+	abscisse = models.IntegerField()
+	ordonnee = models.IntegerField()
+	type = models.CharField(max_length=20)
+	nom = models.CharField(max_length=100)
+	contenu = models.CharField(max_length=10000)
+	envoyer_notification = models.BooleanField()
+	afficher_comme_nouveau = models.BooleanField()
+	date_extraction = models.DateTimeField()
 
